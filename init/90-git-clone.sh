32b22c3458666bec15fdf566783d306c4f226549
Felipe M Barina <felipebarina+git@gmail.com>
2023-08-11 16:11:22 -0300

    config(git): add multicolumn.nvim to git clone

--- init/90-git-clone.sh
+++ init/90-git-clone.sh
@@ -5,10 +5,10 @@
 repo_dir="$DEV_HOME"
 
 git_repos=(
+	'https://github.com/fmbarina/multicolumn.nvim'
 	'https://github.com/b3nj5m1n/xdg-ninja.git'
 	'https://github.com/sudofox/shell-mommy.git'
 	# I'm gonna find a funny use for the last one, just give me time
-	# Also, having this entire script just to clone one repo would be sad
 )
 
 # Run -------------------------------------------------------------------------
